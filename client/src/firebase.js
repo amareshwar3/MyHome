f7db1304a92e361b8b19557e621f9f4ed364dc83
amareshwar3 <amareshwaranampally@gmail.com>
2025-06-21 00:01:32 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-project-fa5d0.firebaseapp.com",
-  projectId: "mern-project-fa5d0",
-  storageBucket: "mern-project-fa5d0.appspot.com",
-  messagingSenderId: "190344990534",
-  appId: "1:190344990534:web:601a4b1561e4af6bc99f47"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
