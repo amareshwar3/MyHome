54f72e56c96db42ed0594eb13a43ebda536f0709
amareshwar3 <amareshwaranampally@gmail.com>
2025-06-21 11:26:22 +0530

    Update verifyUser.js

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,15 +1,14 @@
 import { errorHandler } from "./error.js"
 import jwt from "jsonwebtoken"
 
-export const verifyUser = (req,res,next) => {
-    const token = req.cookies.access_token
+export const verifyUser = (req, res, next) => {
+  const token = req.cookies.access_token || req.headers.authorization?.split(' ')[1];
+  
+  if (!token) return next(errorHandler(401, 'Authentication required'));
 
-    if(!token) return next(errorHandler(401, 'Unauthorized'));
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return next(errorHandler(403, 'Forbidden'));
-    
-        req.user = user;
-        next();
-    });
-}
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, 'Invalid or expired token'));
+    req.user = user;
+    next();
+  });
+};
