7e124ac98da549201b81387f88be167066723669
amareshwar3 <amareshwaranampally@gmail.com>
2025-06-21 12:51:15 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,24 +1,35 @@
-import { configureStore,combineReducers } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const rootReducer = combineReducers({ user: userReducer });
+const rootReducer = combineReducers({ 
+  user: userReducer,
+  // Add other reducers here if needed
+});
 
 const persistConfig = {
   key: 'root',
   storage,
   version: 1,
+  whitelist: ['user'] // Only persist user state
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const errorMiddleware = store => next => action => {
+  if (action.error) {
+    console.error('Redux error:', action.error);
+  }
+  return next(action);
+};
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(errorMiddleware),
 });
 
 export const persistor = persistStore(store);
