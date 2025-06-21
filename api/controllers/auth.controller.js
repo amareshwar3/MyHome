2d3dbf819c8966078fa662409d05fc9bd397456a
amareshwar3 <amareshwaranampally@gmail.com>
2025-06-21 08:48:45 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -11,7 +11,8 @@ export const signup = async(req,res,next) => {
     try {
         await newUser.save()
 
-        res.status(201).json('User created successfully!');
+        res.status(201).json({ success: true, message: 'User created successfully!' });
+
     } catch (error) {
         next(error)
     }
