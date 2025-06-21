fe2a211401f7df792f2b2345826735d45a31a503
amareshwar3 <amareshwaranampally@gmail.com>
2025-06-21 10:40:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target: 'http://localhost:3000',
-        secure: false
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://truehomes-gmvq.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   },
